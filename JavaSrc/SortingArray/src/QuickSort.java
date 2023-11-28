1d9bcc458c1ba4d6d26145e46a0b18029024d1d8
Ivan Fool <frsbscrb@rambler.ru>
2023-11-28 09:00:17 +0300

    javaSB20231128 90017

--- JavaSrc/SortingArray/src/QuickSort.java
+++ JavaSrc/SortingArray/src/QuickSort.java
@@ -9,18 +9,47 @@ public class QuickSort {
         System.out.println();
     }
 
+    private static void swapElements(int[] arr, int beginIndex, int endIndex) {
+        int swap = arr[beginIndex];
+        arr[beginIndex] = arr[endIndex];
+        arr[endIndex] = swap;
+    }
+
+    private static Boolean quickSortStage(int[] arr, int beginIndex, int endIndex) {
+        if ((endIndex - beginIndex) <= 1) {
+            if (endIndex != beginIndex)  {
+                if (arr[beginIndex] > arr[endIndex]) {
+                    swapElements(arr, beginIndex, endIndex);
+                    return true;
+                }
+            }
+        } else {
+// выбираем опрорный элемент
+// пусть будет последний элемент
+         int supportElementIndex = arr.length-1;
+            for (int i = endIndex-beginIndex; i >= 0 ; i--) {
+                if (arr[i] > arr[supportElementIndex]) {
+                    swapElements(arr, i, supportElementIndex);
+                    supportElementIndex = i;
+                    
+                }
+                quickSortStage(arr, beginIndex, supportElementIndex);
+                quickSortStage(arr, supportElementIndex, endIndex);
+            }
+
+
+
+
+        }
+
+
+    }
+
+
     public static void main(String[] args) {
         int[] unsortedArray = {6, 5, 4, 3, 2, 1, 0 };
         printIntArray(unsortedArray);
-        if (unsortedArray.length <= 2) {
-            if (unsortedArray.length == 2) {
-                if (unsortedArray[0] > unsortedArray[1]) {
-                    int swap = unsortedArray[0];
-                    unsortedArray[0] = unsortedArray[1];
-                    unsortedArray[1] = swap;
-                 }
-            }
-        }
+        quickSortStage(unsortedArray, 0,  unsortedArray.length-1);
         printIntArray(unsortedArray);
 
 
