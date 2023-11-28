0d18fbdfe8e517d390e8410fe7c5a34f527c6f72
Ivan Fool <frsbscrb@rambler.ru>
2023-11-28 18:30:43 +0300

    javaSB20231128183043

--- JavaSrc/SortingArray/src/QuickSort.java
+++ JavaSrc/SortingArray/src/QuickSort.java
@@ -1,13 +1,30 @@
 public class QuickSort {
 
-    private static void printIntArray(int[] arr) {
+    private static void printIntArrayIndex(int[] arr, int beginIndex, int endIndex) {
 
-        for (int i = 0; i < arr.length ; i++) {
+        for (int i = beginIndex; i <= endIndex ; i++) {
             System.out.print(arr[i]);
-            if (i < arr.length-1) System.out.print(", ");
+            if (i < endIndex) System.out.print(", ");
         }
         System.out.println();
     }
+
+    private static void printIntArray(int[] arr) {
+        printIntArrayIndex(arr, 0, arr.length-1 );
+//        for (int i = 0; i < arr.length ; i++) {
+//            System.out.print(arr[i]);
+//            if (i < arr.length-1) System.out.print(", ");
+//        }
+//        System.out.println();
+    }
+
+    private static void reLocateElements(int[] arr, int fromIndex, int toIndex, int supportElementIndex) {
+        int swap = arr[fromIndex];
+        arr[fromIndex] = arr[toIndex];
+        arr[toIndex] = arr[supportElementIndex];
+        arr[supportElementIndex] = swap;
+    }
+
 
     private static void swapElements(int[] arr, int beginIndex, int endIndex) {
         int swap = arr[beginIndex];
@@ -15,41 +32,100 @@ public class QuickSort {
         arr[endIndex] = swap;
     }
 
-    private static Boolean quickSortStage(int[] arr, int beginIndex, int endIndex) {
+    private static void quickSortStage(int[] arr, int beginIndex, int endIndex) {
+        System.out.println("Indexes "+ beginIndex + " " + endIndex);
+        // printIntArrayIndex(arr, beginIndex, endIndex);
+
         if ((endIndex - beginIndex) <= 1) {
+            System.out.println("ShortSize!!!");
             if (endIndex != beginIndex)  {
                 if (arr[beginIndex] > arr[endIndex]) {
                     swapElements(arr, beginIndex, endIndex);
-                    return true;
+
                 }
             }
         } else {
 // выбираем опрорный элемент
+// пусть будет первый элемент
+//         int supportElementIndex = beginIndex;
+//         System.out.println("NormalSize!!!");
+//            for (int i = beginIndex; i <= endIndex ; i++) {
+//                System.out.println("For " + i + " " + supportElementIndex + " "+ beginIndex + " "+ endIndex );
+//                if (arr[i] < arr[supportElementIndex]) {
+//                    System.out.println("swap" + i + " " + supportElementIndex );
+//                    swapElements(arr, i, supportElementIndex);
+//                    supportElementIndex = i;
+//                }
+//                System.out.println("AfterFor " + i + " " + supportElementIndex + " "+ beginIndex + " "+ endIndex );
+//
+//            }
+
 // пусть будет последний элемент
-         int supportElementIndex = arr.length-1;
-            for (int i = endIndex-beginIndex; i >= 0 ; i--) {
-                if (arr[i] > arr[supportElementIndex]) {
-                    swapElements(arr, i, supportElementIndex);
-                    supportElementIndex = i;
-                    
+//            int supportElementIndex = endIndex;
+//            System.out.println("NormalSize!!!");
+//            for (int i = endIndex; i >= beginIndex ; i--) {
+//                System.out.println("For " + i + " " + supportElementIndex + " "+ beginIndex + " "+ endIndex );
+//                if (arr[i] > arr[supportElementIndex]) {
+//                    System.out.println("swap" + i + " " + supportElementIndex );
+//                    swapElements(arr, i, supportElementIndex);
+//                    supportElementIndex = i;
+//                }
+//                System.out.println("AfterFor " + i + " " + supportElementIndex + " "+ beginIndex + " "+ endIndex );
+//            }
+// пусть будет случайный элемент
+            int supportElementIndex = beginIndex; //(int)(Math.random()*(endIndex-beginIndex))+beginIndex;
+            System.out.println("NormalSize!!! " + arr[supportElementIndex]);
+// перенос меньших значений влево
+            for (int i = beginIndex; i <= endIndex ; i++) {
+              //  System.out.println("For " + i + " " + supportElementIndex + " "+ beginIndex + " "+ endIndex );
+                if (arr[i] < arr[supportElementIndex]) {
+                    if (i > supportElementIndex) {
+                        if (supportElementIndex < endIndex) {
+                            reLocateElements(arr, i, supportElementIndex + 1, supportElementIndex);
+                            supportElementIndex++;
+
+                        }
+                    }
                 }
-                quickSortStage(arr, beginIndex, supportElementIndex);
-                quickSortStage(arr, supportElementIndex, endIndex);
+
+                System.out.print(i+". ");
+                printIntArrayIndex(arr, beginIndex, endIndex);
+              //  System.out.println("AfterFor " + i + " " + supportElementIndex + " "+ beginIndex + " "+ endIndex );
+            }
+
+            System.out.println("==============");
+
+            for (int i = beginIndex; i <= endIndex ; i++) {
+                //  System.out.println("For " + i + " " + supportElementIndex + " "+ beginIndex + " "+ endIndex );
+
+                if (arr[i] >= arr[supportElementIndex]) {
+                    if (i < supportElementIndex) {
+                    if (supportElementIndex >= beginIndex + 1) {
+                        reLocateElements(arr, i, supportElementIndex - 1, supportElementIndex);
+                        supportElementIndex--;
+                    }
+                }
+                }
+                System.out.print(i+". ");
+                printIntArrayIndex(arr, beginIndex, endIndex);
+                //  System.out.println("AfterFor " + i + " " + supportElementIndex + " "+ beginIndex + " "+ endIndex );
             }
 
 
-
-
+            if (beginIndex < supportElementIndex)
+                quickSortStage(arr, beginIndex, supportElementIndex-1);
+            if (endIndex > supportElementIndex)
+                quickSortStage(arr, supportElementIndex, endIndex);
         }
-
-
     }
 
 
     public static void main(String[] args) {
-        int[] unsortedArray = {6, 5, 4, 3, 2, 1, 0 };
+        int[] unsortedArray = {6, 5, 3, 2, 1, 0 , 4, 7, 11, 9, 8, 8, 17, 15, 12, 25, 33, 77, 158, 160, 10, 14 };
+        System.out.print("Start: ");
         printIntArray(unsortedArray);
         quickSortStage(unsortedArray, 0,  unsortedArray.length-1);
+        System.out.print("End: ");
         printIntArray(unsortedArray);
 
 
