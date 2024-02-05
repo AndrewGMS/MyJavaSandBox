1e8713c27b8dbf938ccb9e7efcc2e8a527becc1a
Ivan Fool <frsbscrb@rambler.ru>
2024-02-05 18:30:41 +0300

    javaSB20240205183041

---
+++ JavaSrc/SortingArray/src/Quiz.java
@@ -0,0 +1,14 @@
+public class Quiz{
+
+
+    public static void main(String[] args) {
+        int a = 0;
+
+        while ((a == a++) & (a < 100)) {
+            System.out.println(++a);
+
+        }
+
+    }
+
+}
