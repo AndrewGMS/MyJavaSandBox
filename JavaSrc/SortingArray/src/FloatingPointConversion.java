1f15e4ff7bbc2085ce526098025eb50f80b72075
Ivan Fool <frsbscrb@rambler.ru>
2024-04-16 18:30:42 +0300

    javaSB20240416183042

---
+++ JavaSrc/SortingArray/src/FloatingPointConversion.java
@@ -0,0 +1,11 @@
+public class FloatingPointConversion {
+
+    public static void main(String[] args) {
+        float f1 = 10;
+//        float f2 = 10.2; // error
+        float f2 = 10.2F;
+        System.out.println(f1+""+f2);
+
+    }
+
+}
