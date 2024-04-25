f3ade3ec19f66e525471e76bb190cc5e96fe960c
Ivan Fool <frsbscrb@rambler.ru>
2024-04-25 18:30:41 +0300

    javaSB20240425183041

--- JavaSrc/SortingArray/src/Quiz.java
+++ JavaSrc/SortingArray/src/Quiz.java
@@ -1,21 +1,28 @@
 public class Quiz{
 
-
+    private String level;
     public static void main(String[] args) {
-        int a = 0;
+        var q = new Quiz();
+        System.out.println("Level:" + q.level);
+    }
 
+    public Quiz() {
+        this("Eazy");
+    }
+
+    public Quiz(String level) {
+      //  this.level = level;
+        level = level;
+    }
+}
+
+
+/*    public static void main(String[] args) {
+        int a = 0;
         while ((a == a++) & (a < 100)) {
             System.out.println(++a);
 
         }
-
         String _My_String;
-
         System.out.println(_My_String);
-
-
-    }
-
-
-
-}
+    } */
