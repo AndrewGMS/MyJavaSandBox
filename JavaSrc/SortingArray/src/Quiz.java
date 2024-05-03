b27909fab07643e3b5cc37d150d3f0779dde2334
Ivan Fool <frsbscrb@rambler.ru>
2024-05-03 18:30:42 +0300

    javaSB20240503183041

--- JavaSrc/SortingArray/src/Quiz.java
+++ JavaSrc/SortingArray/src/Quiz.java
@@ -1,19 +1,59 @@
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Quiz{
-
-    private String level;
-    public static void main(String[] args) {
-        var q = new Quiz();
-        System.out.println("Level:" + q.level);
+    public static final String FILE_NAME = "birds.txt";
+    public static final String NEW_FILE_NAME = "birds-new.txt";
+    public static void main(String[] args) throws IOException {
+        File file = new File(FILE_NAME);
+        try(FileOutputStream out = new FileOutputStream(FILE_NAME)) {
+            out.write("Duck\n".getBytes());
+            out.flush();
+            file.renameTo(new File(NEW_FILE_NAME));
+            out.write("Crow".getBytes());
+        }
+        printFile();
     }
-
-    public Quiz() {
-        this("Eazy");
+    public static void printFile() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(NEW_FILE_NAME))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+        new File(NEW_FILE_NAME).deleteOnExit();
     }
+//    private String level;
+//    public static void main(String[] args) {
+//        var q = new Quiz();
+//        System.out.println("Level:" + q.level);
+//    }
+//
+//    public Quiz() {
+//        this("Eazy");
+//    }
+//
+//    public Quiz(String level) {
+//      //  this.level = level;
+//        level = level;
+//    }
 
-    public Quiz(String level) {
-      //  this.level = level;
-        level = level;
-    }
+
+//    public static void main(String[] args) {
+//        // double d = 5; int i = (int) d;
+//        double d = 5; int i =  d;
+//        short s = 2;
+//        float a = 1.234;
+//        double[] numbers = {10.2, 56.0, 54.7};
+//        Arrays.stream(numbers)
+//                .map(n -> n + 1)
+//                .toArray();
+//        System.out.println(Arrays.toString(numbers));
+//    }
+
 }
 
 
