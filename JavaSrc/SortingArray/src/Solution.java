4bcb1e24c30674c0b1aaf051cf3ff8fce522f9aa
Ivan Fool <frsbscrb@rambler.ru>
2024-05-23 18:30:42 +0300

    javaSB20240523183041

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -1,23 +1,104 @@
+
 import java.util.*;
 
 public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleMap();
+        printPeopleMap(map);
+    }
 
+    public static Map<String, String> createPeopleMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Андреев","Яков");
+        map.put("Богданов","Юрий");
+        map.put("Владимиров","Эдуард");
+        map.put("Григорьев","Харитон");
+        map.put("Дмитриев","Федор");
+        map.put("Егоров","Тимофей");
+        map.put("Зиновьев","Станислав");
+        map.put("Андреев","Юрий");
+        map.put("Богданов","Эдуард");
+        map.put("Владимиров","Андрей");
+        return map;
+    }
 
-
-        public static void main(String[] args) {
-            System.out.println(condition(null) ? 1 : 2);
+    public static void printPeopleMap(Map<String, String> map) {
+        for (Map.Entry<String, String> s : map.entrySet()) {
+            System.out.println(s.getKey() + " " + s.getValue());
         }
+    }
+}
 
-        public static boolean condition(Number o) {
-            return true;
-        }
 
-        public static boolean condition(Long l) {
-            return false;
+
+
+/* public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Dog> dogs = createDogs();
+
+        Set<Object> pets = join(cats, dogs);
+        printPets(pets);
+
+        removeCats(pets, cats);
+        printPets(pets);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> result = new HashSet<>();
+
+        //напишите тут ваш код
+        result.add(new Cat());
+        result.add(new Cat());
+        result.add(new Cat());
+        result.add(new Cat());
+        return result;
+    }
+
+    public static Set<Dog> createDogs() {
+        //напишите тут ваш код
+        Set<Dog> result = new HashSet<>();
+        result.add(new Dog());
+        result.add(new Dog());
+        result.add(new Dog());
+        return result;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+        //напишите тут ваш код
+        Set<Object> result = new HashSet<>();
+        result.addAll(cats);
+        result.addAll(dogs);
+        return result;
+    }
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        //напишите тут ваш код
+        pets.removeAll(cats); // вариант 2 тоже работает
+//            for (Object cat : cats) { // вариант 1
+//                if (pets.contains(cat)) {
+//                    pets.remove(cat);
+//                }
+//        }
+    }
+
+    public static void printPets(Set<Object> pets) {
+        //напишите тут ваш код
+        for (Object pet : pets) {
+            System.out.println(pet);
         }
+        System.out.println();
+    }
+
+    public static class Cat {
 
     }
 
+    public static class Dog {
+
+    }
+}
+*/
  /*   public static void main(String[] args) {
         Set<Cat> cats = createCats();
         Iterator<Cat> itr = cats.iterator();
