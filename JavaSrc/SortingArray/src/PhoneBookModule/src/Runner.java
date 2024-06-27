cc891227fda4b5dcf7c6595650101ba36516325d
Ivan Fool <frsbscrb@rambler.ru>
2024-06-27 18:30:41 +0300

    javaSB20240627183041

---
+++ JavaSrc/SortingArray/src/PhoneBookModule/src/Runner.java
@@ -0,0 +1,20 @@
+import PhoneBook;
+public class Runner {
+
+    PhoneBook phoneBook;
+
+    public void init(){
+        phoneBook = new PhoneBook();
+        phoneBook.addContact();
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
