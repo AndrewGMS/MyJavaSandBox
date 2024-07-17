bf3a0df10833a23b00e9f579cb7f365af3a5196b
Ivan Fool <frsbscrb@rambler.ru>
2024-07-17 18:30:42 +0300

    javaSB20240717183042

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -22,6 +22,41 @@ import  java.util.function.Function;
 import static java.lang.Thread.*;
 
 public class Solution {
+  public static void main(String[] args) {
+    int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+    Pair result = getMinimumMaximumPair(data);
+
+    System.out.println("The minimum is " + result.x);
+    System.out.println("The maximum is " + result.y);
+  }
+
+  public static Pair getMinimumMaximumPair(int[] inputArray) {
+    if (inputArray == null || inputArray.length == 0) {
+      return new Pair(null, null);
+    }
+     int x = inputArray[0];
+     int y = inputArray[0];
+    for (int i = 1; i < inputArray.length; i++) {
+      if (x > inputArray[i]) x = inputArray[i];
+      if (y < inputArray[i]) y = inputArray[i];
+    }
+    return new Pair(x, y);
+  }
+
+  public static class Pair {
+    public Integer x;
+    public Integer y;
+
+    public Pair(Integer x, Integer y) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+}
+
+/*
+public class Solution {
   public static void main(String[] args) throws Exception {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     int[] array = new int[30];
@@ -56,7 +91,7 @@ public class Solution {
 
   }
 }
-
+*/
 /*
 public class Solution {
   public static void main(String[] args) throws IOException {
