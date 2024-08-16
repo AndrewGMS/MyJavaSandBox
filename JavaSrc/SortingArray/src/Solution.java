532b93e595e663b64a832def82b39cfe41ff9f14
Ivan Fool <frsbscrb@rambler.ru>
2024-08-16 18:30:43 +0300

    javaSB20240816183042

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -26,6 +26,126 @@ import  java.util.function.Function;
 
 
 public class Solution {
+    public static List<Thread> threads = new ArrayList<>(5);
+
+    static {
+        threads.add(new Thread1());
+        threads.add(new Thread2());
+        threads.add(new Thread3());
+        threads.add(new Thread4());
+        threads.add(new Thread5());
+
+    }
+
+    public static void main(String[] args){
+
+
+        for (int i = 0; i < threads.size() ; i++) {
+            threads.get(i).start();
+        }
+
+
+
+
+    }
+
+    public static class Thread1 extends Thread{
+        @Override
+        public void run() {
+            while (true) {}
+        }
+    }
+
+    public static class Thread2 extends Thread{
+        @Override
+        public void run() {
+            try {
+                throw new InterruptedException();
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public static class Thread3 extends Thread{
+        @Override
+        public void run() {
+         while (true) {
+             System.out.println("Ура");
+             try {
+             Thread.sleep(500);
+
+             } catch (Exception e) {
+                 e.printStackTrace();
+             }
+
+         }
+
+        }
+    }
+
+    public static class Thread4 extends Thread implements Message{
+        @Override
+        public void showWarning() {
+           this.interrupt();
+        }
+
+        @Override
+        public void run() {
+        while (!isInterrupted()) {}
+
+        }
+    }
+
+    public static class Thread5 extends Thread{
+        @Override
+        public void run() {
+            try {
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                String buf = "";
+                int acc = 0;
+                while (!"N".equals(buf = bufferedReader.readLine())) {
+                    acc += Integer.parseInt(buf);
+                }
+                System.out.println(acc);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
+/*
+Клубок
+1. Создай 5 различных своих нитей (наследников класса Thread):
+1.1. Нить 1 должна бесконечно выполняться;
+1.2. Нить 2 должна выводить "InterruptedException" при возникновении исключения InterruptedException;
+1.3. Нить 3 должна каждые полсекунды выводить "Ура";
+1.4. Нить 4 должна реализовать интерфейс Message, при вызове метода showWarning нить должна останавливаться;
+1.5. Нить 5 должна читать с консоли числа пока не введено слово "N", а потом вывести в консоль сумму введенных чисел.
+2. В статическом блоке добавь свои нити в List<Thread> threads в перечисленном порядке.
+3. Нити не должны стартовать автоматически.
+
+Подсказка:
+Нить 4 можно проверить методом isAlive()
+
+
+Requirements:
+1. Статический блок класса Solution должен создавать и добавлять 5 нитей в список threads.
+2. Нити из списка threads не должны стартовать автоматически.
+3. Нить 1 из списка threads должна бесконечно выполняться.
+4. Нить 2 из списка threads должна выводить "InterruptedException" при возникновении исключения InterruptedException.
+5. Нить 3 из списка threads должна каждые полсекунды выводить "Ура".
+6. Нить 4 из списка threads должна реализовать интерфейс Message, при вызове метода showWarning нить должна останавливаться.
+7. Нить 5 из списка threads должна читать с консоли числа пока не введено слово "N", а потом вывести в консоль сумму введенных чисел.
+
+ */
+
+
+/*
+public class Solution {
     public static byte threadCount = 3;
     static List<Thread> threads = new ArrayList<Thread>(threadCount);
 
@@ -79,7 +199,7 @@ public class Solution {
     }
 }
 
-
+*/
 /*
 Один для всех, все - для одного
 1. Разберись, как работает программа.
