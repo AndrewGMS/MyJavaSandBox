3dd306bd413ac7bb0cd47ed8380b0b04fe94aa0d
Ivan Fool <frsbscrb@rambler.ru>
2024-08-23 18:30:42 +0300

    javaSB20240823183042

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -25,6 +25,143 @@ import java.lang.StackTraceElement;
 import java.util.concurrent.atomic.AtomicInteger;
 import  java.util.function.Function;
 
+
+public class Solution {
+    public static List<Person> allPeople = new ArrayList<Person>();
+
+    static {
+        allPeople.add(Person.createMale("Иванов Иван", new Date()));  //сегодня родился    id=0
+        allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
+    }
+ //   static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+ //   static SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        switch (args[0]) {
+            case "-c": {
+//                -c name sex bd
+                Date bd;
+                Locale locale = new Locale("en");
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", locale);
+                try {
+                    bd = sdf.parse(args[3]);
+                    if (args[2].equals("м")) {
+                        allPeople.add(Person.createMale(args[1], bd));
+                    }
+                    if (args[2].equals("ж")) {
+                        allPeople.add(Person.createFemale(args[1], bd));
+                    }
+                    System.out.println(allPeople.size()-1);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+            break;
+            case "-r": {
+//                -r id
+//                 name sex (м/ж) bd (формат 15-Apr-1990)
+                Locale locale = new Locale("en");
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy", locale);
+
+                int id = Integer.parseInt(args[1]);
+                System.out.println(allPeople.get(id).getName() + " " + (allPeople.get(id).getSex() == Sex.MALE ? "м" : "ж") + " " + sdf.format(allPeople.get(id).getBirthDate()));
+
+            }
+            break;
+            case "-u": {
+//                -u id name sex bd
+                Date bd;
+                Locale locale = new Locale("en");
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", locale);
+                try {
+                    int id = Integer.parseInt(args[1]);
+
+                    bd = sdf.parse(args[4]);
+                    allPeople.get(id).setName(args[2]);
+                    allPeople.get(id).setBirthDate(bd);
+
+
+                    if (args[3].equals("м")) {
+                        allPeople.get(id).setSex(Sex.MALE);
+                    }
+                    if (args[3].equals("ж")) {
+                        allPeople.get(id).setSex(Sex.FEMALE);
+                    }
+
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            break;
+            case "-d": {
+//                -d id
+                int id = Integer.parseInt(args[1] );
+                allPeople.get(id).setName(null);
+                allPeople.get(id).setBirthDate(null);
+                allPeople.get(id).setSex(null);
+
+
+            }
+            break;
+        }
+//        System.out.println(allPeople);
+    }
+}
+
+
+/*
+CRUD
+CRUD - Create, Read, Update, Delete.
+
+Программа запускается с одним из следующих наборов параметров:
+-c name sex bd
+-r id
+-u id name sex bd
+-d id
+
+Значения параметров:
+name - имя, String
+sex - пол, "м" или "ж", одна буква
+bd - дата рождения в следующем формате 15/04/1990
+-c - добавляет человека с заданными параметрами в конец allPeople, выводит id (index) на экран
+-r - выводит на экран информацию о человеке с id: name sex (м/ж) bd (формат 15-Apr-1990)
+-u - обновляет данные человека с данным id
+-d - производит логическое удаление человека с id, заменяет все его данные на null
+
+id соответствует индексу в списке.
+Все люди должны храниться в allPeople.
+Используй Locale.ENGLISH в качестве второго параметра для SimpleDateFormat.
+
+Пример параметров:
+-c Миронов м 15/04/1990
+
+Пример вывода для параметра -r:
+Миронов м 15-Apr-1990
+
+Если программа запущена с параметрами, то они попадают в массив args (аргумент метода main - String[] args).
+Например, при запуске программы c параметрами:
+-c name sex bd
+получим в методе main
+args[0] = "-c"
+args[1] = "name"
+args[2] = "sex"
+args[3] = "bd"
+Для запуска кода с параметрами в IDE IntellijIDEA нужно их прописать в поле Program arguments в меню Run -> Edit Configurations.
+
+
+Requirements:
++1. Класс Solution должен содержать public static поле allPeople типа List<Person>.
++2. Класс Solution должен содержать статический блок, в котором добавляются два человека в список allPeople.
++ 3. При запуске программы с параметром -с программа должна добавлять человека с заданными параметрами в конец списка allPeople, и выводить id (index) на экран.
+4. При запуске программы с параметром -r программа должна выводить на экран данные о человеке с заданным id по формату указанному в задании.
+5. При запуске программы с параметром -u программа должна обновлять данные человека с заданным id в списке allPeople.
+6. При запуске программы с параметром -d программа должна логически удалять человека с заданным id в списке allPeople.
+ */
+
+/*
 public class Solution {
 
     public static void main(String[] args) {
@@ -51,7 +188,7 @@ public class Solution {
     }
 
 }
-
+*/
 /*
 Синхронизированные заметки
 1. Класс Note будет использоваться нитями. Поэтому сделай так, чтобы обращения к листу notes блокировали мьютекс notes, не this
