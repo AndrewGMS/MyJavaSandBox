429fa5009105f1206c3f4b76cc109d4198765e29
Ivan Fool <frsbscrb@rambler.ru>
2024-09-10 18:30:41 +0300

    javaSB20240910183041

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -22,6 +22,68 @@ import  java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        String fileName = bufferedReader.readLine();
+        String fileName = "y:\\MyJavaProjects\\JavaSrc\\SortingArray\\src\\data8.txt";
+        bufferedReader.close();
+
+        File file = new File(fileName);
+        bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+
+        String findLineString = "";
+        while (bufferedReader.ready()) {
+            findLineString = bufferedReader.readLine();
+            if (args[0].equals(findLineString.split(" ")[0])) {
+                break;
+            }
+        }
+        bufferedReader.close();
+        if (args[0].equals(findLineString.split(" ")[0])) System.out.println(findLineString);
+    }
+}
+
+
+
+
+
+
+
+
+/*
+Поиск данных внутри файла
+Считать с консоли имя файла.
+Найти в файле информацию, которая относится к заданному id, и вывести ее на экран в виде, в котором она записана в файле.
+Программа запускается с одним параметром: id (int).
+Закрыть потоки.
+
+В файле данные разделены пробелом и хранятся в следующей последовательности:
+id productName price quantity
+где id - int.
+productName - название товара, может содержать пробелы, String.
+price - цена, double.
+quantity - количество, int.
+
+
+
+Пример содержимого файла:
+194 хлеб 12.6 25
+195 масло сливочное 52.2 12
+196 молоко 22.9 19
+
+Пример вывода для id = 195:
+195 масло сливочное 52.2 12
+
+
+Requirements:
+1. Программа должна считать имя файла с консоли.
+2. В методе main создай для файла поток для чтения.
+3. Программа должна найти в файле и вывести информацию о id, который передается первым параметром.
+4. Поток для чтения из файла должен быть закрыт.
+ */
+
+/*
 
 public class Solution {
     public static void main(String[] args) throws IOException {
@@ -40,7 +102,7 @@ public class Solution {
         }
     }
 }
-
+*/
 
 /*
 
