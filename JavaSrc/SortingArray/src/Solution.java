09759f2980b8061cc2e625e55f7cb4782dd058cc
Ivan Fool <frsbscrb@rambler.ru>
2024-09-12 18:30:44 +0300

    javaSB20240912183044

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -25,27 +25,57 @@ import java.util.function.UnaryOperator;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        FileInputStream fileInputStream = null;
-        String fileName = "";
-        try {
-        while (true) {
-            fileName = bufferedReader.readLine();
-
-            fileInputStream = new FileInputStream(fileName);
-            fileInputStream.close();
-        }
-            } catch (FileNotFoundException e) {
-                System.out.println(fileName);
-            } finally {
-                if (fileInputStream != null) fileInputStream.close();
+           List<String> filesArray = new ArrayList<>();
+//++       BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//---
+            String fileName = "y:\\MyJavaProjects\\JavaSrc\\SortingArray\\src\\datalist.txt";
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+//---
+            String buffFileName;
+            while (!("end".equals(buffFileName = bufferedReader.readLine()))) {
+                filesArray.add(buffFileName);
+//---
+           System.out.println(buffFileName);
+//---
             }
+            bufferedReader.close();
+//--
+        for (String file: filesArray) {
+            System.out.println(file);
+        }
+//--
+
 
 
     }
 }
+/*
+Собираем файл
+Собираем файл из кусочков.
+Считывать с консоли имена файлов.
+Каждый файл имеет имя: [someName].partN.
 
+Например, Lion.avi.part1, Lion.avi.part2, ..., Lion.avi.part37.
+
+
+В папке, где находятся все прочтенные файлы, создать файл без суффикса [.partN].
+
+Например, Lion.avi.
+
+В него переписать все байты из файлов-частей используя буфер.
+Файлы переписывать в строгой последовательности, сначала первую часть, потом вторую, ..., в конце - последнюю.
+Закрыть потоки.
+
+
+Requirements:
++1. Программа должна считывать имена файлов с консоли, пока не будет введено слово "end".
+2. Создай поток для записи в файл без суффикса [.partN] в папке, где находятся все считанные файлы.
+3. В новый файл перепиши все байты из файлов-частей *.partN.
+4. Чтение и запись должны происходить с использованием буфера.
+5. Созданные для файлов потоки должны быть закрыты.
+6. Не используй статические переменные.
+
+ */
 
 
 /*
