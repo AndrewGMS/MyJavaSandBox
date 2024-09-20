5d92c0022aff142e1e217793e576d35d662b3018
Ivan Fool <frsbscrb@rambler.ru>
2024-09-20 18:31:05 +0300

    javaSB20240920183105

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -22,6 +22,146 @@ import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 
+public class Solution{
+    public static Map<String, String> countries = new HashMap<String, String>();
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
+
+
+    public static void main(String[] args) {
+        DataAdapter dataAdapter = new DataAdapter(
+                new Customer() {
+                    private String companyName = "JavaRush Ltd.";
+                    @Override
+                    public String getCompanyName() {
+                        return companyName;
+                    }
+                    private String countryName = "Ukraine";
+                    @Override
+                    public String getCountryName() {
+                        return countryName;
+                    }
+                },
+                new Contact() {
+                    private String name = "Ivanov, Ivan";
+                    @Override
+                    public String getName() {
+                        return name;
+                    }
+                    private String phoneNumber = "+38(050)123-45-67";
+                    @Override
+                    public String getPhoneNumber() {
+                        return phoneNumber;
+                    }
+                }
+        );
+        System.out.println(dataAdapter.getCountryCode());
+        System.out.println(dataAdapter.getCompany());
+        System.out.println(dataAdapter.getContactFirstName());
+        System.out.println(dataAdapter.getContactLastName());
+        System.out.println(dataAdapter.getDialString());
+
+
+
+    }
+
+    public static class DataAdapter implements RowItem {
+        private Customer customer;
+        private Contact contact;
+
+
+        public DataAdapter(Customer customer, Contact contact) {
+            this.customer = customer;
+            this.contact = contact;
+        }
+
+        @Override
+        public String getCountryCode() {
+
+            for (Map.Entry<String, String> entry: countries.entrySet()) {
+              if (entry.getValue().equals(customer.getCountryName())) {
+                  return entry.getKey();
+              }
+            }
+            return null;
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            return contact.getName().split(", ")[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            return contact.getName().split(", ")[0];
+        }
+
+        @Override
+        public String getDialString() {
+//            return "callto://+" + String.format("\d+", contact.getPhoneNumber();
+            return "callto://+" + contact.getPhoneNumber().replaceAll("\\D", "");
+        }
+    }
+
+    public static interface RowItem {
+        String getCountryCode();        //For example: UA
+
+        String getCompany();            //For example: JavaRush Ltd.
+
+        String getContactFirstName();   //For example: Ivan
+
+        String getContactLastName();    //For example: Ivanov
+
+        String getDialString();         //For example: callto://+380501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //For example: JavaRush Ltd.
+
+        String getCountryName();        //For example: Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //For example: Ivanov, Ivan
+
+        String getPhoneNumber();        //For example: +38(050)123-45-67 or +3(805)0123-4567 or +380(50)123-4567 or ...
+    }
+}
+
+/*
+    Закрепляем адаптер
+    Адаптировать Customer и Contact к RowItem.
+        Классом-адаптером является DataAdapter.
+
+        нициализируй countries перед началом выполнения программы. Соответствие кода страны и названия:
+        UA Ukraine
+        RU Russia
+        CA Canada
+
+
+        Requirements:
+    +    1. Класс Solution должен содержать public static поле countries типа Map<String, String>.
+    +    2. В статическом блоке класса Solution инициализируй поле countries тестовыми данными согласно заданию.
+    +    3. Класс Solution должен содержать интерфейс RowItem.
+    +    4. Класс Solution должен содержать интерфейс Contact.
+    +    5. Класс Solution должен содержать интерфейс Customer.
+    +    6. Класс DataAdapter должен реализовывать интерфейс RowItem.
+    +    7. Класс DataAdapter должен содержать два приватных поля: customer типа Customer и contact Contact.
+    +    8. Класс DataAdapter должен содержать конструктор с параметрами (Customer customer, Contact contact), который инициализирует поля contact и customer.
+        9. В классе DataAdapter реализуй методы интерфейса RowItem используя подсказки в виде комментариев в интерфейсах.
+
+*/
+
+/*
 public class Solution {
 
     public static void main(String[] args) throws Exception{
@@ -72,10 +212,8 @@ public class Solution {
     }
 }
 //++
-
-
 }
-
+*/
 
 /*
 еще один адаптер
