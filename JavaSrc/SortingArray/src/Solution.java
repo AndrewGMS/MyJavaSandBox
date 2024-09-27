6075eb492a9ee259aa0e8434eacc42c9641427af
Ivan Fool <frsbscrb@rambler.ru>
2024-09-27 18:30:40 +0300

    javaSB20240927183040

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -24,6 +24,194 @@ import java.util.function.UnaryOperator;
 
 
 public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream =  new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+        testString.printSomething();
+        System.setOut(defaultPrintStream);
+        String s1 = byteArrayOutputStream.toString().replaceAll("[^\\d+*-]","");
+        String[] s2 = s1.split("[+*-]");
+
+        if (s1.indexOf("-") >= 0) s1 = s2[0] + " - " + s2[1] + " = " + (Integer.parseInt(s2[0]) - Integer.parseInt(s2[1]));
+        if (s1.indexOf("+") >= 0) s1 = s2[0] + " + " + s2[1] + " = " + (Integer.parseInt(s2[0]) + Integer.parseInt(s2[1]));
+        if (s1.indexOf("*") >= 0) s1 = s2[0] + " * " + s2[1] + " = " + (Integer.parseInt(s2[0]) * Integer.parseInt(s2[1]));
+        System.out.println(s1);
+//        System.out.println(byteArrayOutputStream.toString().replaceAll("[^\\d+*-]",""));
+//        System.out.println(byteArrayOutputStream.toString().replaceAll("\\D",""));
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("3 + 6 = ");
+        }
+    }
+}
+
+
+/*
+Решаем пример
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна выводить на консоль решенный пример.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+
+Возможные операции: + - *
+Шаблон входных данных и вывода: a [знак] b = c
+Отрицательных и дробных чисел, унарных операторов - нет.
+
+Пример вывода:
+3 + 6 = 9
+
+
+Requirements:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "3 + 6 = ".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c параметром конструктора ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out.
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething() согласно заданию, и выводить её в консоль.
+ */
+
+/*
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream =  new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+        testString.printSomething();
+        System.setOut(defaultPrintStream);
+        System.out.println(byteArrayOutputStream.toString().replaceAll("[^\\d+]",""));
+        System.out.println(byteArrayOutputStream.toString().replaceAll("\\D",""));
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
+*/
+/*
+ыводим только цифры
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна выводить только цифры.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+
+Пример вывода:
+12345678
+
+
+Requirements:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "it's 1 a 23 text 4 f5-6or7 tes8ting".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c параметром конструктора ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out.
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething() согласно заданию, и выводить её в консоль.
+ */
+
+/*
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream =  new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+        testString.printSomething();
+        System.setOut(defaultPrintStream);
+        System.out.println(byteArrayOutputStream.toString().replaceAll("te","??"));
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+*/
+
+/*
+Ридер обертка 2
+В методе main подмените объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна заменять все подстроки "te" на "??".
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+
+
+Requirements:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "it's a text for testing".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c параметром конструктора ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething() согласно заданию, и выводить её в консоль.
+ */
+
+/*
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream defaultPrintStream = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream =  new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+        testString.printSomething();
+
+        System.setOut(defaultPrintStream);
+        System.out.println(byteArrayOutputStream.toString().toUpperCase());
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
+*/
+/*
+Ридер обертка
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна преобразовывать весь текст в заглавные буквы.
+Вызови готовый метод printSomething(), воспользуйся testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+
+
+Requirements:
++ 1. Класс Solution должен содержать класс TestString.
++ 2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
++ 3. Класс TestString должен содержать публичный void метод printSomething().
++ 4. Метод printSomething() класса TestString должен выводить на экран строку "it's a text for testing".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c конструктором принимающим ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out.
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething() согласно заданию, и выводить её в консоль.
+ */
+
+/*
+public class Solution {
     public static void main(String[] args) throws IOException{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 //            String fileName1 = bufferedReader.readLine();
@@ -48,7 +236,7 @@ public class Solution {
     }
 }
 
-
+*/
 /*
 Пунктуация
 Считать с консоли 2 имени файла.
