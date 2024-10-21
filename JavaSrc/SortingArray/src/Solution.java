57217a08aebb968a084d9905b7c61f1cd656d509
Ivan Fool <frsbscrb@rambler.ru>
2024-10-21 18:30:43 +0300

    javaSB20241021183043

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -26,49 +26,42 @@ public class Solution {
     public static Map<Integer, String> map = new HashMap<Integer, String>();
 
     static {
-        map.put(0, "ноль");
-        map.put(1, "один");
-        map.put(2, "два");
-        map.put(3, "три");
-        map.put(4, "четыре");
-        map.put(5, "пять");
-        map.put(6, "шесть");
-        map.put(7, "семь");
-        map.put(8, "восемь");
-        map.put(9, "девять");
-        map.put(10, "десять");
-        map.put(11, "одиннадцать");
-        map.put(12, "двенадцать");
-    }
-
-
-
-    public static void main(String[] args) throws Exception{
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        String fileName1 = bufferedReader.readLine();
-        String fileName1 = "y:\\MyJavaProjects\\JavaSrc\\SortingArray\\src\\data22.txt";
-        bufferedReader.close();
-
-        BufferedReader bufferedReader1 = new BufferedReader(new FileReader(fileName1));
-        String[] buffer2;
-        String buffer;
-        while (bufferedReader1.ready()) {
-            int countOccurrences = 0;
-            buffer = bufferedReader1.readLine();
-//            buffer2 = buffer.split(" ");
-////            System.out.println(buffer2.length);
-//            for (int i = 0; i < buffer2.length; i++) {
-//                for (int j = 0; j < words.size(); j++) {
-//                    System.out.println(words.get(j) + " " + buffer2[i]);
-//                    if (words.get(j).equals(buffer2[i])) countOccurrences++;
-//                }
-//            }
-////            System.out.println(countOccurrences);
-//            if (countOccurrences == 2)
-//                System.out.println(buffer);
+            map.put(0, "ноль");
+            map.put(1, "один");
+            map.put(2, "два");
+            map.put(3, "три");
+            map.put(4, "четыре");
+            map.put(5, "пять");
+            map.put(6, "шесть");
+            map.put(7, "семь");
+            map.put(8, "восемь");
+            map.put(9, "девять");
+            map.put(10, "десять");
+            map.put(11, "одиннадцать");
+            map.put(12, "двенадцать");
         }
-        bufferedReader1.close();
+
+
+
+        public static void main(String[] args) throws Exception{
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String fileName1 = bufferedReader.readLine();
+//        String fileName1 = "y:\\MyJavaProjects\\JavaSrc\\SortingArray\\src\\data23.txt";
+            bufferedReader.close();
+
+            BufferedReader bufferedReader1 = new BufferedReader(new FileReader(fileName1));
+
+            while (bufferedReader1.ready()) {
+
+                String buffer = bufferedReader1.readLine();
+
+                for (Map.Entry<Integer, String> mapUnit: map.entrySet()) {
+                    buffer = buffer.replaceAll("\\b" + mapUnit.getKey() +  "\\b", mapUnit.getValue());
+                }
+                System.out.println(buffer);
+            }
+            bufferedReader1.close();
     }
 }
 
