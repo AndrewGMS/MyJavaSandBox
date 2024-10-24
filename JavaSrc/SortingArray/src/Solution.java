88f0cb1be44bc5fd2b888a6726760c3caa4c8241
Ivan Fool <frsbscrb@rambler.ru>
2024-10-24 18:30:45 +0300

    javaSB20241024183044

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -24,6 +24,93 @@ import java.util.function.UnaryOperator;
 
 
 public class Solution {
+        public static void main(String[] args) {
+                //исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+                try {
+                        File your_file_name = File.createTempFile("your_file_name", null);
+                        OutputStream outputStream = new FileOutputStream(your_file_name);
+                        InputStream inputStream = new FileInputStream(your_file_name);
+
+                        Human ivanov = new Human("Ivanov", new Asset("home", 999_999.99), new Asset("car", 2999.99));
+                        ivanov.save(outputStream);
+                        outputStream.flush();
+
+                        Human somePerson = new Human();
+                        somePerson.load(inputStream);
+                        inputStream.close();
+                        //check here that ivanov equals to somePerson - проверьте тут, что ivanov и somePerson равны
+
+                } catch (IOException e) {
+                        //e.printStackTrace();
+                        System.out.println("Oops, something wrong with my file");
+                } catch (Exception e) {
+                        //e.printStackTrace();
+                        System.out.println("Oops, something wrong with save/load method");
+                }
+        }
+
+        public static class Human {
+                public String name;
+                public List<Asset> assets = new ArrayList<>();
+
+                public Human() {
+                }
+
+                public Human(String name, Asset... assets) {
+                        this.name = name;
+                        if (assets != null) {
+                                this.assets.addAll(Arrays.asList(assets));
+                        }
+                }
+
+                @Override
+                public boolean equals(Object o) {
+                        if (this == o) return true;
+                        if (o == null || getClass() != o.getClass()) return false;
+
+                        Human human = (Human) o;
+
+                        if (name != null ? !name.equals(human.name) : human.name != null) return false;
+                        return assets != null ? assets.equals(human.assets) : human.assets == null;
+                }
+
+                @Override
+                public int hashCode() {
+                        int result = name != null ? name.hashCode() : 0;
+                        result = 31 * result + (assets != null ? assets.hashCode() : 0);
+                        return result;
+                }
+
+                public void save(OutputStream outputStream) throws Exception {
+                        //implement this method - реализуйте этот метод
+                }
+
+                public void load(InputStream inputStream) throws Exception {
+                        //implement this method - реализуйте этот метод
+                }
+        }
+}
+
+/*
+
+Читаем и пишем в файл: Human
+Часто необходимо сохранять состояние работы программы. До появления сериализации каждый делал это своим способом. В этой задаче нужно сохранить в файл состояние работы программы и вычитать состояние из файла без использования сериализации.
+
+Реализуй логику записи в файл и чтения из файла для класса Human.
+Поле name в классе Human не может быть пустым.
+Метод main реализован только для тебя и не участвует в тестировании.
+
+
+Requirements:
+1. Логика чтения/записи реализованная в методах save/load должна работать корректно в случае, если список assets пустой.
+2. Логика чтения/записи реализованная в методах save/load должна работать корректно в случае, если поле name и список assets не пустые.
+3. Класс Solution.Human не должен поддерживать интерфейс Serializable.
+4. Класс Solution.Human должен быть публичным.
+5. Класс Solution.Human не должен поддерживать интерфейс Externalizable.
+ */
+
+/*
+public class Solution {
         {
                 System.out.println("This is the Solution class");
         }
@@ -57,8 +144,8 @@ public class Solution {
                                 }
                         }
 
-                        if (result instanceof A) {
-                                A p = (A) result;
+                        if (result instanceof C) {
+                                C p = (C) result;
                                 System.out.println(p.getClass().getSimpleName());
                         }
 
@@ -90,7 +177,7 @@ public class Solution {
 
 }
 
-
+*/
 /*
 справить ошибку. Классы и интерфейсы
 Программа содержит всего 1 логическую ошибку.
