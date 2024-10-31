5145ac40f0a0f0d0d1c271aae37f759e8b55ea00
Ivan Fool <frsbscrb@rambler.ru>
2024-10-31 18:30:42 +0300

    javaSB20241031183042

--- JavaSrc/SortingArray/src/Solution.java
+++ JavaSrc/SortingArray/src/Solution.java
@@ -23,6 +23,88 @@ import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 
+
+public class Solution {
+        public static void main(String[] args) {
+                //you can find your_file_name.tmp in your TMP directory or adjust outputStream/inputStream according to your file's actual location
+                //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+                try {
+                        File yourFile = File.createTempFile("your_file_name", null);
+                        OutputStream outputStream = new FileOutputStream(yourFile);
+                        InputStream inputStream = new FileInputStream(yourFile);
+
+                        JavaRush javaRush = new JavaRush();
+                        //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
+                        javaRush.save(outputStream);
+                        outputStream.flush();
+
+                        JavaRush loadedObject = new JavaRush();
+                        loadedObject.load(inputStream);
+                        //here check that the javaRush object is equal to the loadedObject object - проверьте тут, что javaRush и loadedObject равны
+                        System.out.println(loadedObject.equals(javaRush));
+
+                        outputStream.close();
+                        inputStream.close();
+
+                } catch (IOException e) {
+                        //e.printStackTrace();
+                        System.out.println("Oops, something is wrong with my file");
+                } catch (Exception e) {
+                        //e.printStackTrace();
+                        System.out.println("Oops, something is wrong with the save/load method");
+                }
+        }
+
+        public static class JavaRush {
+                public List<User> users = new ArrayList<>();
+
+                public void save(OutputStream outputStream) throws Exception {
+                        //implement this method - реализуйте этот метод
+                        System.out.println("Saving...");
+
+
+                }
+
+                public void load(InputStream inputStream) throws Exception {
+                        //implement this method - реализуйте этот метод
+                        System.out.println("Lading...");
+                }
+
+                @Override
+                public boolean equals(Object o) {
+                        if (this == o) return true;
+                        if (o == null || getClass() != o.getClass()) return false;
+
+                        JavaRush javaRush = (JavaRush) o;
+
+                        return users != null ? users.equals(javaRush.users) : javaRush.users == null;
+
+                }
+
+                @Override
+                public int hashCode() {
+                        return users != null ? users.hashCode() : 0;
+                }
+        }
+}
+
+/*
+Читаем и пишем в файл: JavaRush
+Реализуй логику записи в файл и чтения из файла для класса JavaRush.
+Пустых полей у объекта User быть не может. Дату в файле удобно хранить в формате long.
+Метод main реализован только для тебя и не участвует в тестировании.
+
+
+Requirements:
+1. Логика чтения/записи реализованная в методах save/load должна работать корректно в случае, если список users пустой.
+2. Логика чтения/записи реализованная в методах save/load должна работать корректно в случае, если список users не пустой.
+3. Класс Solution.JavaRush не должен поддерживать интерфейс Serializable.
+4. Класс Solution.JavaRush должен быть публичным.
+5. Класс Solution.JavaRush не должен поддерживать интерфейс Externalizable.
+ */
+
+
+/*
 public class Solution {
         public static void main(String[] args) {
                 //исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
@@ -39,7 +121,7 @@ public class Solution {
                         somePerson.load(inputStream);
                         inputStream.close();
                         //check here that ivanov equals to somePerson - проверьте тут, что ivanov и somePerson равны
-
+                        System.out.println(ivanov.equals(somePerson));
                 } catch (IOException e) {
                         //e.printStackTrace();
                         System.out.println("Oops, something wrong with my file");
@@ -82,16 +164,40 @@ public class Solution {
                 }
 
                 public void save(OutputStream outputStream) throws Exception {
-                        outputStream.write(name.getBytes());
                         //implement this method - реализуйте этот метод
+                        System.out.println("Save");
+                        PrintWriter printWriter = new PrintWriter(outputStream);
+                        System.out.println(this.name);
+                        printWriter.println(this.name);
+                        if (this.assets.size() > 0)
+                            for (Asset asset: this.assets) {
+                                    System.out.println(asset.getName() + " - " + asset.getPrice());
+                                printWriter.println(asset.getName());
+                                printWriter.println(asset.getPrice());
+                        }
+                        printWriter.close();
+
+
                 }
 
                 public void load(InputStream inputStream) throws Exception {
                         //implement this method - реализуйте этот метод
+                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                        this.name = bufferedReader.readLine();
+                        System.out.println(this.name);
+                        while (bufferedReader.ready()) {
+                                String assetName = bufferedReader.readLine();
+                                double assetPrice = Double.parseDouble(bufferedReader.readLine());
+                                this.assets.add(new Asset(assetName, assetPrice));
+                        }
+                        bufferedReader.close();
+
                 }
         }
 }
 
+*/
 /*
 
 Читаем и пишем в файл: Human
